f7bf64deb2235854a4ae3c8d590a05d04c5df2ae
ping58972 <ping58972@gmail.com>
2019-06-01 02:29:18 -0500

    warn up mysql query

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,33 +5,34 @@ const Cart = require('../modles/cart');
 
 
 exports.getProducts = (req, res, next)=>{
-    Product.fetchAll(products => {
-        res.render('shop/product-list', 
-     {prods: products,
-      pageTitle: 'All Product', 
-      path: '/products'
-     });
+    Product.fetchAll().then(([rows, fieldData]) =>{
+       res.render('shop/product-list', 
+    {prods: rows,
+     pageTitle: 'All Product', 
+     path: '/products'
     });
+    }).catch(err=>console.log(err));
  }
 exports.getProduct = (req, res, next)=>{
    const prodId = req.params.productId;
-   Product.findById(prodId, product => {
+   Product.findById(prodId).then(([product])=>{
+      //console.log(product);
       res.render('shop/product-detail', 
-      {product: product, 
-         pageTitle: product.title, 
+      {product: product[0], 
+         pageTitle: product[0].title, 
          path:'/products'
       });
-   });
+   }).catch(err=>console.log(err));
  }
 
  exports.getIndex = (req, res, next) => {
-    Product.fetchAll(products => {
-        res.render('shop/index', 
-     {prods: products,
-      pageTitle: 'Shop', 
-      path: '/'
-     });
+    Product.fetchAll().then(([rows, fieldData]) => {
+       res.render('shop/index', 
+    {prods: rows,
+     pageTitle: 'Shop', 
+     path: '/'
     });
+    }).catch(err=>console.log(err));
  }
  exports.getCart = (req, res, next) => {
     Cart.getCart(cart => {
--- modles/product.js
+++ modles/product.js
@@ -1,22 +1,6 @@
-
-const fs = require('fs');
-const path = require('path');
-
+const db = require('../util/database');
 const Cart = require('./cart');
 
-const p = path.join(
-        path.dirname(process.mainModule.filename), 
-        'data', 
-        'products.json');
-const getProductsFromFile = callBack => {
-        fs.readFile(p, (err, fileContent)=>{
-            if(err){
-                callBack([]);
-            } else {
-                callBack(JSON.parse(fileContent));
-            }
-        })
-}
 
 module.exports = class Product {
     constructor(id, title, imageUrl, description, price) {
@@ -28,46 +12,20 @@ module.exports = class Product {
     }
 
     save() {
+      return  db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+        [this.title, this.price, this.imageUrl, this.description]
+        );
         
-        getProductsFromFile(products =>{
-            if(this.id){
-                const existingProductIndex = products.findIndex(prod => prod.id === this.id);
-                const updatedProducts = [...products];
-                updatedProducts[existingProductIndex] = this;
-                fs.writeFile(p, JSON.stringify(updatedProducts), (err)=>{
-                    console.log(err);
-                });
-            } else {
-                this.id = Math.random().toString();
-                products.push(this);
-                fs.writeFile(p, JSON.stringify(products), (err)=>{
-                    console.log(err);
-                });
-            }
-        });
-       // fs.readFile(p, (err, fileContent)=> {});
     }
 
    static deleteById(id) {
-        getProductsFromFile(products => {
-            const product = products.find(prod => prod.id === id);
-            const updatedProducts = products.filter(p=> p.id !== id);
-            fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-                if(!err){
-                    Cart.deleteProduct(id, product.price)
-                }
-            });
-        });
+        
     }
 
-    static fetchAll(callBack) {
-        getProductsFromFile(callBack);
-       
+    static fetchAll() {
+        return db.execute('SELECT * FROM products');
     }
-    static findById(id, callBack) {
-        getProductsFromFile(products => {
-            const product = products.find(p=> p.id === id);
-            callBack(product);
-        });
+    static findById(id) {
+      return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
     }
 }
